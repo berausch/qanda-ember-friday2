e6f93d2dad24307c660ae2a206c874d71c7805d7
Brittany Rausch <berausch247@gmail.com>
2016-08-19 11:44:36 -0700

    Create the new-question and new-answer components and content

---
+++ app/components/new-answer.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+
+    save1() {
+      var params = {
+        answerContent: this.get('answerContent') ? this.get('answerContent') : "",
+        authorAnswer: this.get('authorAnswer') ? this.get('authorAnswer') : "",
+        questionContent: this.get('questionContent') ? this.get('questionContent') : "",
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('save2', params);
+    }
+  }
+});
---
+++ app/components/new-question.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow(){
+      this.set('addNewQuestion', true);
+    },
+    saveQuestion() {
+      var params = {
+        questionContent: this.get('questionContent') ? this.get('questionContent'): "",
+        authorQuestion: this.get('authorQuestion') ? this.get('authorQuestion'): "",
+        notes: this.get('notes') ? this.get('notes'): ""
+
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
+});
