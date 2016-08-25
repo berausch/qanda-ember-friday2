eb70b0c821856f232a6fdec134b846abe6ac6edc
Brittany Rausch and George Olson <brittany+george@>
2016-08-25 14:23:34 -0700

    Create beginning upvote funcitonality

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,9 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  voteNumber: function(){
+    if(this.get('answer').votes === 0){
+      this.set('voteNumber', false);
+    } else {
+      this.set('voteNumber', true);
+    }
+  },
   actions: {
     upvoteAnswer(answer) {
-      answer.incrementProperty("vote");
-    }
+      answer.incrementProperty("votes");
+
+      }
   }
 });
