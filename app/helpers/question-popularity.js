3db7cb083cae3b2b630a457d56f36017aaed7dcf
Brittany Rausch and George Olson <brittany+george@>
2016-08-25 13:15:39 -0700

    Created the helper object that wil display a flame next to questions with more than 3 answers. Added functionality in index

---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export function questionPopularity(params) {
+  var question = params[0];
+
+  if(question.get('answers').get('length') >= 3) {
+    return Ember.String.htmlSafe('<img src"/assets/images/flame.png">');
+  }
+
+
+}
+
+export default Ember.Helper.helper(questionPopularity);
